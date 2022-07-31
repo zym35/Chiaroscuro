fd014137e4e0efb8681260e40d8e78a72ae7d821
King Zhou <zhouyimingking@outlook.com>
2022-07-31 00:53:55 -0700

    Character movement v2

--- Assets/Scipts/StickPlayerMovement.cs
+++ Assets/Scipts/StickPlayerMovement.cs
@@ -1,18 +1,25 @@
-using System;
+using DG.Tweening;
 using UnityEngine;
 
-[ExecuteInEditMode]
 public class StickPlayerMovement : MonoBehaviour
 {
     private const float PlayerHeight = 3;
 
     public float stepLengthMultiplier = 0.06f;
-    public float speed;
+    public float stepThreshold = 0.5f;
+    public float bumpHeightMultiplier = 0.1f;
+    public float bodyTiltMultiplier = 0.3f;
+    public float speed, rotateSpeed;
     public Transform leftFootIK, rightFootIK;
-
-    public bool _lastFootLeft = true;
+    public Transform leftKnee, rightKnee;
+    public Transform body;
 
     private Rigidbody _rigidbody;
+    private bool _isFootMoving;
+    private Vector3 _leftDest, _rightDest;
+    private float _stepLength;
+    private Vector3 _targetPosition = Vector3.zero;
+    private Quaternion _targetRotation = Quaternion.identity;
 
     private void Awake()
     {
@@ -21,104 +28,141 @@ public class StickPlayerMovement : MonoBehaviour
 
     private void Update()
     {
-        Vector3 playerPos = transform.position;
-        Vector3 footCenter = (leftFootIK.position + rightFootIK.position) / 2;
-        Debug.DrawLine(playerPos, footCenter, Color.yellow);
-        if (Vector3.Distance(playerPos, footCenter) > Vector3.Distance(leftFootIK.position, footCenter))
-        {
-            StepPolar(leftFootIK.position, rightFootIK.position, playerPos,
-                out Vector3 leftPos, out Vector3 rightPos);
-            if (!_lastFootLeft)
-            {
-                leftFootIK.position = leftPos;
-            }
-            else
-            {
-                rightFootIK.position = rightPos;
-            }
-
-            _lastFootLeft = !_lastFootLeft;
-            Debug.Break();
-        }
+        CreateStepBox();
+        TiltBody(_stepLength * bodyTiltMultiplier);
+        RaycastGround();
+        DebugStepBox(_leftDest, _rightDest, transform.TransformPoint(0.5f, 0, 0), transform.TransformPoint(-0.5f, 0, 0));
+        MoveFeetIKs();
+        CheckKnee();
     }
 
     private void FixedUpdate()
     {
-        Move();
+        MovePosition();
+        MoveRotation();
+
+        _rigidbody.MovePosition(_targetPosition);
+        _rigidbody.MoveRotation(_targetRotation);
     }
 
-    private void LogDegree(float radius, string n)
+    private void RaycastGround()
     {
-        Debug.Log(n + ": " + radius * Mathf.Rad2Deg);
+        Vector3 leftDest = transform.TransformPoint(-0.5f, 1, _stepLength);
+        Vector3 rightDest = transform.TransformPoint(0.5f, 1, _stepLength);
+
+        if (Physics.Raycast(new Ray(leftDest, Vector3.down), out RaycastHit hitLeft, 5))
+        {
+            leftDest = hitLeft.point;
+        }
+        else
+        {
+            leftDest = transform.TransformPoint(-0.5f, 0, _stepLength);
+        }
+
+        if (Physics.Raycast(new Ray(rightDest, Vector3.down), out RaycastHit hitRight, 5))
+        {
+            rightDest = hitRight.point;
+        }
+        else
+        {
+            rightDest = transform.TransformPoint(0.5f, 0, _stepLength);
+        }
+
+        _leftDest = leftDest;
+        _rightDest = rightDest;
     }
 
-    private void StepPolar(Vector3 leftFootPos, Vector3 rightFootPos, Vector3 playerPos, out Vector3 newLeftPos, out Vector3 newRightPos)
+    private void CheckKnee()
     {
-        Vector3 upAxis = Vector3.up;
-        float r0 = Vector3.Distance(leftFootPos, rightFootPos) / 2;
-        float r1 = 2 * r0;
-        Vector3 axisOrigin = (leftFootPos + rightFootPos) / 2;
-        Vector3 axisFront = Vector3.Cross(upAxis, leftFootPos - axisOrigin);
-        float playerAngle = Vector3.SignedAngle(axisFront, playerPos - axisOrigin, upAxis) * Mathf.Deg2Rad;
-        float triangleCornerAngle = Mathf.Acos(r0 / r1);
+        if (Physics.Raycast(leftKnee.position + Vector3.up, Vector3.down, 1.1f) ||
+            Physics.Raycast(rightKnee.position + Vector3.up, Vector3.down, 1.1f))
+        {
+            if (_isFootMoving) return;
 
-        LogDegree(playerAngle, "playerAngle");
-        LogDegree(triangleCornerAngle, "triangleCornerAngle");
-
-        float newLeftIKAngle = Mathf.PI / 2 - triangleCornerAngle - playerAngle;
-
-        LogDegree(newLeftIKAngle, "newLeftIKAngle");
-
-        Vector3 newLeftPosLocal = new(-r1 * Mathf.Sin(newLeftIKAngle), 0, r1 * Mathf.Cos(newLeftIKAngle));
-        Vector3 newRightPosLocal = new(r1 * Mathf.Sin(newLeftIKAngle), 0, r1 * Mathf.Cos(newLeftIKAngle));
-
-        Debug.DrawLine(newLeftPosLocal, newRightPosLocal, Color.yellow);
-
-        //newLeftPos = Quaternion.AngleAxis(-playerAngle, upAxis) * newLeftPosLocal + axisOrigin;
-        newLeftPos = newLeftPosLocal + axisOrigin;
-        newRightPos = newRightPosLocal + axisOrigin;
+            float leftDist = Vector3.Distance(_leftDest, leftFootIK.position);
+            float rightDist = Vector3.Distance(_rightDest, rightFootIK.position);
+            if (leftDist > rightDist)
+            {
+                MoveFoot(leftFootIK, _leftDest);
+            }
+            else
+            {
+                MoveFoot(rightFootIK, _rightDest);
+            }
+        }
     }
 
     private void CreateStepBox()
     {
         Vector3 velocity = _rigidbody.velocity;
+
         float dot = Vector3.Dot(transform.forward, velocity);
-        float stepLength = velocity.magnitude * stepLengthMultiplier;
+        _stepLength = velocity.magnitude * stepLengthMultiplier;
         if (dot == 0)
-            stepLength = 0;
+            _stepLength = 0;
         else if (dot < 0)
-            stepLength *= -1;
-
-        Vector3 lowLeft = leftFootIK.position;
-        Vector3 upLeft = leftFootIK.TransformPoint(0, 0, stepLength);
-        Vector3 upRight = rightFootIK.TransformPoint(0, 0, stepLength);
-        Vector3 lowRight = rightFootIK.position;
-
-        DebugStepBox(lowLeft, upLeft, upRight, lowRight);
+            _stepLength *= -1;
     }
 
-    private void Move()
+    private void MoveFeetIKs()
     {
-        float horizontal = Input.GetAxis("Horizontal");
-        float vertical = Input.GetAxis("Vertical");
-        Vector3 input = speed * Time.deltaTime * new Vector3(horizontal, 0, vertical);
+        float stepSpeedThreshold = stepThreshold * (1 + _rigidbody.velocity.magnitude);
+        float leftDist = Vector3.Distance(_leftDest, leftFootIK.position);
+        float rightDist = Vector3.Distance(_rightDest, rightFootIK.position);
 
-        Vector3 movement = transform.TransformPoint(input);
-        _rigidbody.MovePosition(movement);
+        if (leftDist > stepSpeedThreshold && rightDist > stepSpeedThreshold)
+        {
+            if (_isFootMoving) return;
+
+            if (leftDist > rightDist)
+            {
+                MoveFoot(leftFootIK, _leftDest);
+            }
+            else
+            {
+                MoveFoot(rightFootIK, _rightDest);
+            }
+        }
+    }
+
+    private async void MoveFoot(Transform ik, Vector3 dest)
+    {
+        _isFootMoving = true;
+        float duration = _rigidbody.velocity.magnitude < 0.5f ? 0.15f : 0.5f / _rigidbody.velocity.magnitude;
+        await ik.DOJump(dest, 0.2f, 1, duration).AsyncWaitForCompletion();
+        _isFootMoving = false;
+    }
+
+    private void MovePosition()
+    {
+        float vertical = Input.GetAxis("Vertical");
+        if (Input.GetKey(KeyCode.LeftShift))
+            vertical *= 2;
+        Vector3 input = speed * Time.deltaTime * new Vector3(0, 0, vertical);
+
+        _targetPosition = transform.TransformPoint(input);
+
+        float targetY = Mathf.Min(leftFootIK.position.y, rightFootIK.position.y);
+        _targetPosition.y = Mathf.Lerp(transform.position.y, targetY, 0.1f);
+    }
+
+    private void MoveRotation()
+    {
+        float horizontal = Input.GetAxis("Horizontal") * Time.deltaTime * rotateSpeed;
+        _targetRotation = Quaternion.AngleAxis(transform.rotation.eulerAngles.y + horizontal, Vector3.up);
     }
 
     private void TiltBody(float length)
     {
         float angle = Mathf.Rad2Deg * Mathf.Asin(length / PlayerHeight);
-
-        transform.rotation = Quaternion.Euler(angle, 0, 0);
+        body.transform.localRotation = Quaternion.Euler(angle, 0, 0);
     }
 
     private void DebugStepBox(Vector3 p1, Vector3 p2, Vector3 p3, Vector3 p4)
     {
-        Debug.DrawLine(p1, p2, Color.green);
-        Debug.DrawLine(p2, p3, Color.green);
-        Debug.DrawLine(p3, p4, Color.green);
-        Debug.DrawLine(p4, p1, Color.green);
+        Debug.DrawLine(p1, p2, Color.yellow);
+        Debug.DrawLine(p2, p3, Color.yellow);
+        Debug.DrawLine(p3, p4, Color.yellow);
+        Debug.DrawLine(p4, p1, Color.yellow);
     }
 }
