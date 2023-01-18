54de97c11fb9104d8a6084870797b547a84940b4
King Zhou <zhouyimingking@outlook.com>
2023-01-18 11:33:09 -0800

    projector

--- Assets/Scipts/PlatformerPlayer.cs
+++ Assets/Scipts/PlatformerPlayer.cs
@@ -5,28 +5,39 @@ using UnityEngine;
 
 public class PlatformerPlayer : MonoBehaviour
 {
-    public float speed = 1;
+    public float speed = 2;
     public float jumpSpeed = 1;
-
-    private Rigidbody _rigidbody;
-
-    private void Awake()
-    {
-        _rigidbody = GetComponent<Rigidbody>();
-    }
+    public float size = .5f;
+    public float gravity = 9;
 
     void Update()
     {
         float horizontal = Input.GetAxis("Horizontal");
-        //float vertical = Input.GetAxis("Vertical");
+        float vertical = Input.GetAxis("Vertical");
 
-        Vector3 movement = horizontal * speed * Time.deltaTime * Vector3.right;
-
+        Vector3 movement =  new Vector3(horizontal * speed * Time.deltaTime, 0, 0);
+        movement += Vector3.down * gravity * Time.deltaTime;
         if (Input.GetKeyDown(KeyCode.W))
         {
-            _rigidbody.AddForce(Vector3.up * jumpSpeed, ForceMode.Impulse);
+            movement += Vector3.up * jumpSpeed;
         }
 
-        transform.Translate(movement, Space.World);
+        if (TestDestination(transform.position + movement.normalized * size + movement))
+        {
+            transform.Translate(movement, Space.World);
+        }
+    }
+
+    private bool TestDestination(Vector3 pos)
+    {
+
+        if (Physics.Raycast(pos, Vector3.back, 100))
+        {
+            return false;
+        }
+        else
+        {
+            return true;
+        }
     }
 }
